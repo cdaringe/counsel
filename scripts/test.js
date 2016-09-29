0adae1efccd1484b72cbefd3dd9d23b5a04a1717
cdaringe <christopher.dieringer@gmail.com>
2016-09-29 01:38:19 -0700

    :art: simplify bulk test runner.

--- scripts/test.js
+++ scripts/test.js
@@ -1,63 +1,18 @@
 'use strict'
 
+require('perish')
+const fs = require('fs')
+const path = require('path')
+const execSync = require('child_process').execSync
+
 /**
  * Test.
  *
- * Run all packages' tests in parallel
+ * Run all packages' tests in series
  */
-const async = require('async')
-const exec = require('shelljs').exec
-
-const children = []
-
-function getExec(dirPath, callback) {
-  const child = exec('npm test', {
-    async: true,
-    cwd: dirPath,
-    silent: false,
-  }, (exitCode, stdout, stderr) => {
-    if (exitCode) {
-      callback(stderr.toString())
-    } else {
-      callback(null, stdout.toString())
-    }
-  })
-
-  children.push(child)
-
-  return child
-}
-
-function killChildren() {
-  children.forEach(c => c.kill())
-}
-
-process.on('exit', killChildren)
-
-async.series([
-  cb1 => {
-    const children = []
-    const dirPaths = [
-      './packages/counsel',
-      './packages/counsel-common'
-    ]
-
-    async.parallel(dirPaths.map(dirPath => {
-      return cb1a => children.push(getExec(dirPath, cb1a))
-    }), (error, response) => {
-      if (error) {
-        cb1(error)
-      } else {
-        cb1(null, response)
-      }
-    })
-  }
-], (error, results) => {
-  killChildren() // Ensure child processes are killed
-
-  if (error) {
-    console.error(error)
-  } else {
-    console.log(results)
-  }
+const pkgDirnames = fs.readdirSync(path.resolve(__dirname, '..', 'packages')).filter(p => path.basename(p)[0] !== '.')
+pkgDirnames.map(dir => {
+  const cwd = path.resolve(__dirname, '..', 'packages', dir)
+  console.log(`running tests in: ${cwd}`)
+  execSync('npm test', { cwd })
 })
