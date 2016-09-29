042063a6968084d1db166536c96ba59368664568
cdaringe <christopher.dieringer@gmail.com>
2016-09-29 13:31:34 -0700

    :bug: de-swallow-ify test errs.

--- test/index.js
+++ test/index.js
@@ -11,12 +11,8 @@ const execSync = require('child_process').execSync
  * Run all packages' tests in series
  */
 const pkgDirnames = fs.readdirSync(path.resolve(__dirname, '..', 'packages')).filter(p => path.basename(p)[0] !== '.')
-pkgDirnames.map(dir => {
+pkgDirnames.map((dir, ndx) => {
   const cwd = path.resolve(__dirname, '..', 'packages', dir)
   console.log(`running tests in: ${cwd}`)
-  try {
-    console.log(execSync('npm test', { cwd: cwd }).toString())
-  } catch (err) {
-    console.error(err)
-  }
+  console.log(execSync('npm test', { cwd: cwd }).toString())
 })
